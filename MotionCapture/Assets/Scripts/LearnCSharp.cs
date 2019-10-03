6c6cb2890a7f52c0acf57a4ac8f3a5c71b7fad3a
KID <50666207+KID421@users.noreply.github.com>
2019-10-03 09:06:23 +0800

    認識欄位

---
+++ MotionCapture/Assets/Scripts/LearnCSharp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LearnCSharp : MonoBehaviour
+{
+    // 欄位 Field
+    // 欄位類型 欄位名稱 (指定 值)；
+    int score = 80;         // 整數
+    float speed = 1.5f;     // 浮點數
+    string prop = "藥水";   // 字串
+    bool mission = false;   // 布林值 true/false
+}
