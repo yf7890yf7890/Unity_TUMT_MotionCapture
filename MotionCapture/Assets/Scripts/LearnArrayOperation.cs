67a950f346ac060ba8ac2e89b0dbf1016a518830
KID <50666207+KID421@users.noreply.github.com>
2019-10-17 09:35:50 +0800

    陣列存取與取得陣列長度

--- MotionCapture/Assets/Scripts/LearnArrayOperation.cs
+++ MotionCapture/Assets/Scripts/LearnArrayOperation.cs
@@ -9,4 +9,16 @@ public class LearnArrayOperation : MonoBehaviour
 
     // 陣列
     public int[] students = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+
+    private void Start()
+    {
+        // 取得陣列資料
+        Debug.Log(students[6]);
+
+        // 存入陣列資料
+        students[2] = 33;
+
+        // 陣列數量 (長度)
+        Debug.Log("學生陣列的長度：" + students.Length);
+    }
 }
