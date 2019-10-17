a0a331ebfea570fa0891b32f59061d3ef32ff70d
KID <50666207+KID421@users.noreply.github.com>
2019-10-17 09:17:46 +0800

    新增學習陣列腳本與新增分數陣列

---
+++ MotionCapture/Assets/Scripts/LearnArrayOperation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LearnArrayOperation : MonoBehaviour
+{
+    public int studentA = 10;
+    public int studentB = 20;
+    public int studentC = 30;
+
+    // 陣列
+    public int[] students = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+}
