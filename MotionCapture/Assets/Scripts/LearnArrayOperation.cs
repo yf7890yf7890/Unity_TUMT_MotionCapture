807920bd69985a8273491e051b704144f0f82d73
KID <50666207+KID421@users.noreply.github.com>
2019-10-17 10:03:30 +0800

    數學與比較運算子

--- MotionCapture/Assets/Scripts/LearnArrayOperation.cs
+++ MotionCapture/Assets/Scripts/LearnArrayOperation.cs
@@ -10,6 +10,8 @@ public class LearnArrayOperation : MonoBehaviour
     // 陣列
     public int[] students = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
 
+    public Color[] colors = { Color.black, Color.red };
+
     private void Start()
     {
         // 取得陣列資料
@@ -20,5 +22,20 @@ public class LearnArrayOperation : MonoBehaviour
 
         // 陣列數量 (長度)
         Debug.Log("學生陣列的長度：" + students.Length);
+
+        // 數學運算子
+        Debug.Log(10 + 3);
+        Debug.Log(10 - 3);
+        Debug.Log(10 * 3);
+        Debug.Log(10 / 3);
+        Debug.Log(10 % 3);
+
+        // 比較運算子 (結果都是布林值)
+        Debug.Log(10 > 3);      // t
+        Debug.Log(10 < 3);      // f
+        Debug.Log(10 >= 3);     // t
+        Debug.Log(10 <= 3);     // f
+        Debug.Log(10 == 3);     // f
+        Debug.Log(10 != 3);     // t
     }
 }
